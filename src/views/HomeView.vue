487dc225ba8d0e18446b741001844d26e7c5d922
resna1505 <resnapangestu01@gmail.com>
2022-06-09 03:06:35 +0700

    upload project laravue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <HeroShayna/>
+    <WomanShayna/>
+    <InstagramShayna/>
+    <PartnerShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomanShayna from '@/components/WomanShayna.vue'
+import InstagramShayna from '@/components/InstagramShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+// import carousel from 'vue-owl-carousel'
+
+export default {
+  name: 'HomeView',
+    components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna,
+    InstagramShayna,
+    PartnerShayna,
+    FooterShayna
+}
+}
+</script>
